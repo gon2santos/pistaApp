95091ef8e1308651847541a62243d9bf2bda0785
gon2santos <necchigonzalo@gmail.com>
2023-07-12 16:00:58 -0300

    impl rtkQuery

---
+++ redux/apiSlices/apiSlices.js
@@ -0,0 +1,19 @@
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+// Define a service using a base URL and expected endpoints
+export const pistaApi = createApi({
+    reducerPath: 'pistaApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://127.0.0.1:3000'
+    }),
+    endpoints: (builder) => ({
+        getPistaByName: builder.query({
+            query: () => `carruseles/`,
+        }),
+    }),
+})
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetPistaByNameQuery } = pistaApi
--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './slices/counterSlice'
+import { pistaApi } from './apiSlices/apiSlices'
 
 export const store = configureStore({
     reducer: {
+        [pistaApi.reducerPath]: pistaApi.reducer,
         counter: counterReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(pistaApi.middleware),
 })
